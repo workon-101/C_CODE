25bf60879c0bb2384da1403c088bfce1ab0eb10a
workon <falcon8988@gmail.com>
2025-02-01 05:00:45 +0800

    before test

---
+++ test3.cpp
@@ -0,0 +1,99 @@
+
+#include<stdio.h>
+
+int globle = 2024;
+
+void test(){
+	printf("test()-- %d\n", globle);
+}
+int main(void){
+	test();
+	printf("%d\n", globle);
+	return 0;
+}
+
+//int main(){
+//	int num = 0;
+//	{
+//		printf("%d\n", num);
+//	}
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = 0;
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//int a = 10;
+
+//int main(){
+//	{
+//	    
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int a = 100;
+
+//int main(){
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int num2 = 20;
+
+//int main(){
+//	int num1 = 10;
+//	return0;
+//}
+
+//int main(){
+//	short age = 17;
+//	float weight = 49.8f;
+//	return 0;
+//}
+
+//int main()
+//{
+
+	
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main(){
+	
+	//char ch = 'A';
+	//printf ("%c\n", ch);
+	//int age = 17;
+	//printf("%d\n", age);
+	//long num = 100;
+	//printf("%d\n", num);
+	//float f = 5.7887885;
+	//printf("%f\n", f);
+	//double d = 3.1415926;
+	//printf("%lf\n", d);
+	//return 0;
+//}
+
+//#include<stdio.h>
+//int main(){
+//	printf("hehe\n");
+//	
+//	return 0;
+//}
