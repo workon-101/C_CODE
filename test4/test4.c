c2d710393eae7a61f32576a7b3744852846cbd1c
workon <falcon8988@gmail.com>
2025-01-31 16:45:19 +0800

    testgithub

---
+++ test4/test4.c
@@ -0,0 +1,43 @@
+//#include <iostream>
+#include<stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	const int n = 10;
+	int arr[n] = {0};
+	//const int num = 4;
+	//printf("%d\n", num);
+	//num = 8;
+	//printf("%d\n", num);
+	//3// zhimianchanglian
+	//100;
+	//3.1415926;
+	return 0;
+}
+
+//int main(){
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+//int main(){
+//	{
+//		int a = 10;
+//		printf("%d\n", a);//ok
+//	}
+//	printf("%d\n", a);//error
+//	return 0;
+//}
+
+//int main(int argc, char** argv) {
+//	extern int g_val;
+//	printf("g_val = %d\n", g_val);
+//	return 0;
+//}
